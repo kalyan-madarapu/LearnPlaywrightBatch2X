ad527ebda8ab7eb3113e42c7db6db7f39a74961d
Local Snapshot <local-snapshot@example.com>
2026-06-21 23:07:15 +0530

    Fix import paths and typo in 169_utils.js

---
+++ Chapter_20_Typescript_Basics/169_utils.js
@@ -0,0 +1,5 @@
+import { BASEURL as bul_util, formatTestName } from "./utils.js";
+import { BASE_URL as bul_testurl, formatUpperCaseString } from "./testutils.js";
+console.log(bul_util);
+console.log(bul_testurl);
+console.log(formatTestName("login"));
